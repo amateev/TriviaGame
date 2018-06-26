c11b185a32455a21ce063f3ba03bcaf6362f353a
Alexandra M <alexandramatveyeva@yahoo.com>
2018-06-26 15:23:27 -0700

    pseudocode created

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+// 1. function: on click of a START btn:
+// hide: START btn 
+// hide: card subtitle 
+// add: countdown timer starting from 30 min
+// add: Q string and radiobuttons GUI elements
+// 3.function: 
+// i. if correct choice, add to Correct Answer
+// ii. if incorrect choice, add to Incorrect Answer
+// iii. if no btns chosen points added to Unanswered
+// add: btn DONE
+
+// 2. function: time is 0 or/and on click of a DONE btn:
+// hide: countdown timer
+// hide: Q and radiobuttons
+// hide: btn DONE
+// add: 'All Done!' string
+// add: Correct Answer total points 
+// add: Incorrect Answer total points 
+// add: Unanswered total points 
