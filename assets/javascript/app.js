b53587d939bae966477f56351ff24733e35945b3
Alexandra M <alexandramatveyeva@yahoo.com>
2018-06-27 11:38:13 -0700

    set timer, add done btn with id, add hide functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,39 +1,63 @@
 // set time of 30 sec
-const countDownStart = 30;
+var countDownStart = 30;
 
-// 1. function: on click of a START btn:
-$('.success').on('click', function () {
+
+
+//1. function: on click of a START btn:
+$('#start_btn').on('click', function () {
     // alert("hello");
+    
     // hide: START btn
     $('.success').hide();
     // hide: card subtitle
     $(".card-subtitle").hide();
-    // Trigger and add: countdown timer starting from 30 sec
-    function countDownTimer() {
-        var countDownCurrent = countDownStart--;
-        var paragraph = $("<p>").addClass('countdown');
-        var timer = paragraph.html(countDownCurrent + "seconds");
-        $(".countdown").append(timer);
-        return;
-    }
-
-
-})
-
-
-
+    //add: countdown timer starting from 30 sec
+    var countDownCurrent = countDownStart;
+    
+    //Display timer by appending a div in .card-text
+    var paragraph = $("<div>").addClass('countdown');
+    var timer = paragraph.html(countDownCurrent + " seconds");
+    $(".card-text").append(timer);
+    j  = countDownStart - countDownCurrent;
+    countDownCurrent--; 
 // add: Q string and radiobuttons GUI elements
 // 3.function: 
 // i. if correct choice, add to Correct Answer
 // ii. if incorrect choice, add to Incorrect Answer
 // iii. if no btns chosen points added to Unanswered
-// add: btn DONE
 
-// 2. function: time is 0 or/and on click of a DONE btn:
-// hide: countdown timer
+    // add: btn DONE
+    var newbuttonDone = $("<button>").addClass('btn success').html('DONE');
+    var buttonDone = newbuttonDone.attr('onclick', 'setTimeout(myFunction, 2000);').attr('id', 'done_btn');
+    $(".card-text").append(buttonDone);
+    
+     
+   
+    
+
+   
+   
+  
+
+    if (countDownCurrent == 0) {
+
+    }
+})
+// 2. Trigger countdown condition and click of a DONE btn condition:
+function myFunction() {
+        //alert('hello');
+    // hide: countdown timer
+    $(".countdown").hide();
 // hide: Q and radiobuttons
 // hide: btn DONE
+$("#done_btn").hide();
 // add: 'All Done!' string
 // add: Correct Answer total points 
 // add: Incorrect Answer total points 
 // add: Unanswered total points 
+    }
+
+
+
+
+
